534f5a062ecc15f216b34640360007b085c508ea
Kamil Kondratowicz <prywatny3@o2.pl>
2016-10-21 14:48:13 +0200

    Refactoring & added remove button

--- js/main.js
+++ js/main.js
@@ -1,90 +1,72 @@
-(function (d) {
+(function(d) {
     'use strict';
 
-    function ProductDesc(name, how_much, weight) {
-        this.name = name;
-        this.how_much = how_much;
-        this.weight = weight;
-    }
-    
-    ProductDesc.prototype.addProduct = function () {
-        var list = d.getElementById("products"),
-            newElement = d.createElement("li");
-        
-        list.appendChild(newElement);
-        newElement.classList.add("product");
-        
-        newElement.innerHTML += `<p class='nameProduct'>${this.name}</p>
-                                    <div class='product-moreinfo'>
-                                        <div class='product-info'>Ilość: ${this.how_much} szt.</div>
-                                        <div class='product-info'>Waga: ${this.weight} g</div>
+    class Product {
+        constructor(name, howMuch, weight){
+            this.name = name;
+            this.howMuch = howMuch;
+            this.weight = weight;
+            this.deleteProduct();
+        }
+        addProduct() {
+            const list = d.getElementById('products'),
+                newElement = d.createElement('li');
+
+            list.appendChild(newElement);
+
+            newElement.classList.add('list__item');
+            newElement.classList.add('product');
+            newElement.innerHTML = `<p class='product__name'>${this.name}</p>
+                                    <button class='product__remove'>x</button>
+                                    <div class='product__info'>
+                                        <div>Ilość: ${this.howMuch} szt.</div>
+                                        <div>Waga: ${this.weight} g.</div>
                                     </div>`;
-    };
-    
-    ProductDesc.prototype.searchProduct = function () {
-        var products = d.querySelectorAll(".nameProduct"),
-            len = products.length,
-            result = true;
-        
-        if (len < 1) {
-            result = false;
-        } else {
-            for (var i = 0; i < len; i++) {
-                if (products[i].innerHTML.toLowerCase() === this.name.toLowerCase()) {
-                    result = true;
-                    break;
-                } else {
-                    result = false;
+        }
+        searchProduct() {
+            const products = d.querySelectorAll('.product__name'),
+                len = products.length;
+            let result = true,
+                i;
+
+            if (len < 1) {
+                result = false;
+            } else {
+                for (i = 0; i < len; i++) {
+                    if (products[ i ].innerHTML.toLowerCase() === this.name.toLowerCase()) {
+                        result = true;
+                        break;
+                    } else {
+                        result = false;
+                    }
                 }
             }
+            return result;
         }
-        return result;
-    };
-    
-    d.getElementById("btn-add").addEventListener("click", function () {
-        var nameProduct = d.getElementById("product-name").value,
-            how_muchProduct = d.getElementById("product-how_much").value,
-            weightProduct = d.getElementById("product-weight").value,
-            product = new ProductDesc(nameProduct, how_muchProduct, weightProduct);
+        deleteProduct() {
+            d.getElementById('products').addEventListener('click', (e) => {
+                const t = e.target;
+                if (t.className === 'product__remove') {
+                    t.parentNode.remove();
+                }
+            }, false);
+        }
+    }
+    d.getElementById('btn-add').addEventListener('click', () => {
+        const nameProduct = d.getElementById('product-name').value,
+            howMuchProduct = d.getElementById('product-how_much').value,
+            weightProduct = d.getElementById('product-weight').value,
+            product = new Product(nameProduct, howMuchProduct, weightProduct);
 
-        if (nameProduct !== "") {
+        if (nameProduct !== '') {
             if (product.searchProduct()) {
-                window.alert("Taki produkt widnieje już na Twojej liście!");
+                window.alert('Taki produkt widnieje już na Twojej liście!');
             } else {
                 product.addProduct();
             }
         } else {
-            window.alert("Nie podano nazwy produktu!");
+            window.alert('Nie podano nazwy produktu!');
         }
     }, false);
-    
-    d.getElementById("products").addEventListener("click", function (event) {
-        var t = event.target;
-        if (t.className === "nameProduct") {
-            t.parentNode.parentNode.removeChild(t.parentNode);
-        }
-    }, false);
-    
-}(document));
 
-    /*
-    function searchProduct(nameProduct) {
-        var products = d.querySelectorAll(".nameProduct"),
-            len = products.length,
-            result = false;
-        
-        if (len < 1) {
-            result = true;
-        } else {
-            for (var i = 0; i < len; ++i) {
-                if (products[i].innerHTML === nameProduct) {  
-                    result = false;
-                    return;
-                } else {
-                    result = true;
-                }
-            }
-        }
-        return result;
-    }
-    */
+}(document));
